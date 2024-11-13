40b07f9317bdda2cce929c451396869c0e955958
pc <pc@email.com>
2024-11-13 09:25:32 +0900

    test

---
+++ js/test.js
@@ -0,0 +1,22 @@
+let sum = 0;
+// for(let i = 1;i<=10;i++){
+//   sum += i
+// }
+// console.log(`1~10 까지의 합 : ${sum}`)
+
+//2. 구구단
+
+// let dan = 4;
+// for(let i = 1; i < 10; i++){
+//   console.log(`(${dan} * ${i} = ${dan*i})`)
+// }
+
+// let numAry = [23, 66, 92, 71, 52, 49];
+
+// for (let i = 0; i < numAry.length; i++) {
+//   if (numAry[i] % 2 == 1) {
+//     sum += numAry[i]
+//   }
+// }
+// console.log(`홀 수의 합 : ${sum}`)
+
